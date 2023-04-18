f47fc0fffac21b343d583e970421a9521cb13a9b
Drashti Mahyavanshi <21cs024@charusat.edu.org>
2023-04-18 19:41:53 +0530

    first commit

---
+++ Ticket_Reservation_system/booking/faqs.php
@@ -0,0 +1,17 @@
+
+<div class="ui accordion">
+  <div class="title" onclick="$('#c1').slideToggle();">
+    <i class="dropdown icon"></i>
+    What is an #Order Ref?
+  </div>
+  <div class="content" id="c1">
+    <p class="transition" >An Order Reference Number is a unique system generated number that identifies your ticket as well as booking information .</p>
+  </div>
+  <div class="title" onclick="$('#c2').slideToggle();">
+    <i class="dropdown icon"></i>
+    Can i retrieve or re-print a lost ticket?
+  </div>
+  <nbsp;>
+  <div class="content" id="c2">
+    <p class="transition">Yes|True, its possible, just provide the #Order ref no above and the system will validate and re-print your ticket.</p>
+  </div>
